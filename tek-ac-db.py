bea46c8acfee0202d6f92862b2983a4a352c82e0
acamilo <alex.camilo@gmail.com>
2016-03-07 23:54:56 -0500

    Swapped out Database with peewee orm

--- tek-ac-db.py
+++ tek-ac-db.py
@@ -1,8 +1,13 @@
 from flask import Flask, request, redirect, url_for, send_from_directory, render_template
 from flask.ext.basicauth import BasicAuth
-import pickledb
-import sys
+from datetime import date
 
+import sys
+import os
+
+from peewee import *
+
+db = SqliteDatabase(sys.argv[3])
 
 app = Flask(__name__)
 app.debug = True
@@ -10,9 +15,32 @@ app.debug = True
 app.config['BASIC_AUTH_USERNAME'] = sys.argv[1]
 app.config['BASIC_AUTH_PASSWORD'] = sys.argv[2]
 
-db = pickledb.load(sys.argv[3], True)
-
 basic_auth = BasicAuth(app)
+
+# Set up some objects for or ORM
+class IDCard(Model):
+  first    = CharField()
+  last     = CharField()
+  cardsite = IntegerField()
+  cardnum  = IntegerField()
+  front    = BooleanField()
+  back     = BooleanField()
+  wood     = BooleanField()
+  metal    = BooleanField()
+  
+  class Meta:
+    database = db
+
+class Access(Model):
+  item   = CharField()
+  result = CharField()
+  date   = DateField()
+  card   = ForeignKeyField(IDCard, related_name='events')
+  
+  class Meta:
+    database = db
+
+
 
 # This function decodes the binary string sent to us by the card reader
 # for our 33 bit wiegand card. 7 bit site.
@@ -41,70 +69,46 @@ def convertWieg(wiegbits):
 @app.route('/')
 @basic_auth.required
 def root():
-  return render_template('index.html', cards=db.db)
+  return render_template('index.html', cards=IDCard.select())
 
 # Update card data
 @app.route('/update', methods=['POST'])
 def update():
+  
   cardid = request.form['card']
-  first  = request.form['first']
-  last   = request.form['last']
-  
-  front  = request.form.get('front','')
-  if front == '':
-    front = False;
-  else:
-    front = True;
-  back   = request.form.get('back','')
-  if back == '':
-    back = False;
-  else:
-    back = True;
-  wood   = request.form.get('wood','')
-  if wood == '':
-    wood = False;
-  else:
-    wood = True;
-  metal  = request.form.get('metal','')
-  if metal == '':
-    metal = False;
-  else:
-    metal = True;
-  
-  currcard = {'first': first, 'last': last, 'front':front, 'back':back, 'wood':wood, 'metal':metal}
-  
-  print cardid
-  print currcard
-  db.set(cardid,currcard);
-  db.dump();
-  return render_template('index.html', cards=db.db, currcard=currcard, cardid=cardid)
+  # Lookup card by ID
+  currcard = IDCard.get(IDCard.cardnum == cardid)
+  # update info
+  currcard.first  = request.form['first']
+  currcard.last   = request.form['last']
+  currcard.front  = request.form.get('front','') == 'on'
+  currcard.back   = request.form.get('back','')  == 'on'
+  currcard.wood   = request.form.get('wood','')  == 'on'
+  currcard.metal  = request.form.get('metal','') == 'on'
+  # Save to DB
+  currcard.save()
+  return render_template('index.html', cards=IDCard.select(), currcard = currcard)
 
 # View card details  
 @app.route('/view/<path:card>')
 def view(card):
-  currcard = db.get(card)
-  return render_template('index.html', cards=db.db, currcard=currcard,cardid=card)
+  currcard = IDCard.get(IDCard.cardnum == card)
+  return render_template('index.html', cards=IDCard.select(),currcard = currcard)
 
 # Add Card  
 @app.route('/add', methods=['POST'])
 def add():
-  newcard=request.form['newcard']
-  db.set(newcard,{'first': '', 'last': '', 'front':False, 'back':False, 'wood':False, 'metal':False})
-  db.dump();
-  
-  currcard = db.get(newcard);
-  return render_template('index.html', cards=db.db, currcard=currcard,cardid=newcard)
+  cardnum=request.form['newcard']
+  # Create new card and push to DB
+  newcard = IDCard(first='',last='',cardsite=17,cardnum=cardnum,front=False,back=False,wood=False,metal=False)
+  newcard.save()
+  return render_template('index.html', cards=IDCard.select(), currcard = newcard)
+
 # Serve Static Files
 @app.route('/<path:path>')
 @basic_auth.required
 def static_proxy(path):
   return app.send_static_file(path)
-
-# Send a list of cards.
-@app.route("/db")
-@basic_auth.required
-def getcarddb():
-  return "Hello World!"
 
 # Validate Card and then do a lookup in the DB.
 # Check to see if card has permission to access.
@@ -114,15 +118,23 @@ def access(card,action):
   try:
     c = convertWieg(card) # validate card data
     if c['site'] == 17:   # check site code
-      if db.get(str(c['card']))[action] == True:
+      currcard = IDCard.get(IDCard.cardnum == c['card'])
+      if getattr(currcard,action):
         return 'approved' # Tell reader it can open door
-  except KeyError:
-    return 'denied' # Unknown card. Dont open.
-  except TypeError:
-    return 'denied' # Unknown card. Dont open.
-  except ValueError:
+  except:
     return 'denied' # Invalid Data. Don't open.
   return 'denied'   # Card lacks permissions. Don't open.
 
 if __name__ == "__main__":
+  #Does DB exist?
+  if os.path.isfile(sys.argv[3])==False:
+    print "Initializing empty database."
+    db.connect()
+    db.create_tables([IDCard, Access])
+  else:
+    print "Opening existing database"
+    db.connect()
+  
+  
+    
   app.run(host='0.0.0.0', port=8080)
